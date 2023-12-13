f1e983425228037d17d07ff4088ce75fbcdf831c
Verena <>
2023-12-13 09:52:26 +0100

    poke-1 adding new stuff

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-let currentPokemon = [];
+let currentPokemon;
 let allFetchedPokemon = [];
+
+
+async function getSynonyms() {
+    let query = document.getElementById('searchQuery').value;
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    let response = await fetch(url);
+    let responseAsJson = await response.json();
+    let synsets = responseAsJson['synsets'];
+    renderSynsets(synsets);
+}
+
+function renderSynsets(synsets) {
+    let container = document.getElementById('container');
+
+    container.innerHTML = `<div> Es wurden <b>${synsets.length}</b>Synonym-Sets geladen.</div>`;
+
+    for (let i = 0; i < synsets.length; i++) {
+        const synset = synsets[i];
+        let terms = synset['terms']; // ist ein Array
+
+        container.innerHTML += `<h2>Synonym- Set mit ID ${synset['id']}</h2>`;
+
+        for (let j = 0; j < terms.length; j++) {
+            const term = terms[j];
+            container.innerHTML += `<div> ${term['term']}</h2>`;
+        }
+    }
+}
 
 async function pokedexHome() {
     // all Pokemons
@@ -7,29 +35,63 @@ async function pokedexHome() {
     let response = await fetch(url);
     let responseAsJson = await response.json();
     let pokemonList = responseAsJson['results'];
-
+    allFetchedPokemon.push(pokemonList);
+    
     for (let i = 1; i < pokemonList.length; i++) {
     let singleUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
     let singleResponse = await fetch(singleUrl);
     let singleResponseAsJson = await singleResponse.json();
-    let pokemonName = singleResponseAsJson['name'];
-    document.getElementById('pokemon-cards').innerHTML += `<div class="pokemon-single-card" id="pokemon-single-card-${i}"><h2>${pokemonName}</h2></div>`;
+    let currentPokemon = singleResponseAsJson['name'];
+    let currentPokemonImageSrc = singleResponseAsJson['sprites']['other']['official-artwork']['front_default'];
+    let currentPokemonImageSrcLittle = singleResponseAsJson['sprites']['front_default'];
+    capitalizedType = currentPokemon.charAt(0).toUpperCase() + currentPokemon.slice(1)
+    document.getElementById('pokemon-cards').innerHTML += renderSinglePokemon(i, currentPokemonImageSrcLittle, capitalizedType, currentPokemonImageSrc);
+        for (let j = 0; j < singleResponseAsJson['types'].length; j++) {
+
+            let currentPokemonType = singleResponseAsJson['types'][j]['type']['name'];
+            document.getElementById(`pokemon-type-${i}`).innerHTML += `
+           
+            <div class="types">${currentPokemonType}</div>
+            </div>`;
+        }
     }
 }
 
-/* async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
-    let response = await fetch(url);
-    let currentPokemon = await response.json();
-    let currentPokemonImage = currentPokemon['sprites']['other']['dream_world']['front_default'];
-    console.log('Loaded Pokemon', currentPokemon);
-    renderPokemonInfo(currentPokemon, currentPokemonImage);
+function renderSinglePokemon(i,currentPokemonImageSrcLittle, capitalizedType, currentPokemonImageSrc) {
+    return `
+    <div class="pokemon-single-card" id="pokemon-single-card-${i}">
+        <img class="pokemon-image-little" src="${currentPokemonImageSrcLittle}"/>
+        <h2>${capitalizedType}</h2>
+        <img class="pokemon-image" src="${currentPokemonImageSrc}"/>
+        <div id="pokemon-type-${i}" class="types-container">
+        </div>
+    </div>
+    `;
 }
 
-function renderPokemonInfo(currentPokemon, currentPokemonImage) {
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemonImage').src = currentPokemonImage;
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
-    
 
- */
+function checkType(typeContainer, pokemonType) {
+    let typeColors = {
+      grass: "bg_green",
+      poison: "bg_lightGreen",
+      flying: "bg_gray",
+      fire: "bg_red",
+      electric: "bg_yellow",
+      ground: "bg_brown",
+      normal: "bg_beige",
+      water: "bg_blue",
+      bug: "bg_lime",
+      fairy: "bg_pink",
+      fighting: "bg_orange",
+      psychic: "bg_purple",
+      rock: "bg_ligthGray",
+      steel: "bg_silver",
+      ice: "bg_lightBlue",
+      ghost: "bg_lightPurple",
+    };
+    let colorClass = typeColors[pokemonType] || "bg_default";
+    typeContainer.classList.add(colorClass);
+  }
