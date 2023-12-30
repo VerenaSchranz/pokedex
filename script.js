62051e79fc7a24c95e20a670e12c545fc1829c83
Verena <>
2023-12-30 19:29:39 +0100

    fixed searchbar and code clean up and improve the design for mobile view

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 let pokemonMap = {};
 let allFetchedPokemon = [];
+let allSearchedPokemon = [];
 pokemonOffSet = 0;
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Add an event listener to the search input
+  document.getElementById("search").addEventListener("input", filterPokemon);
+});
 
+async function filterPokemon() {
+  let searchTerm = document.getElementById("search").value.toLowerCase();
 
+  // Clear existing Pokemon cards
+  document.getElementById("pokemon-cards").innerHTML = "";
 
-// JavaScript
-async function searchPokemon() {
-    let search = document.getElementById("search").value.toLowerCase();
-    let pokemonCard = document.getElementById("pokemon-cards");
-    pokemonCard.innerHTML = "";
+  // Clear previous searched data
+  allSearchedPokemon = [];
 
-    for (let i = 1 + pokemonOffSet; i < 30 + pokemonOffSet; i++) {
-        let pokemonName = pokemonMap[i]["name"];
+  // Filter Pokemon based on the search term
+  for (let i = 0; i < allPokemon.length; i++) {
+    let currentPokemon = allPokemon[i].name;
 
-        if (pokemonName.toLowerCase().includes(search)) {
-            renderSinglePokemon(i);
-        }
+    if (currentPokemon.toLowerCase().includes(searchTerm)) {
+      let index = i + 1 + pokemonOffSet;
+      let singleUrl = `https://pokeapi.co/api/v2/pokemon/${index}`;
+      let singleResponse = await fetch(singleUrl);
+      let singleResponseAsJson = await singleResponse.json();
+      pokemonMap[index] = singleResponseAsJson;
+
+      // Check if the Pokemon name is already in allSearchedPokemon
+      if (!allSearchedPokemon.includes(singleResponseAsJson.name)) {
+        allSearchedPokemon.push(singleResponseAsJson.name);
+        document.getElementById("pokemon-cards").innerHTML +=
+          renderSinglePokemon(index);
+      }
     }
+  }
 }
 
-
-
-async function loadPokemon() {
+async function loadAllPokemon() {
   // Render each Pokemon
   for (let i = 1 + pokemonOffSet; i < 30 + pokemonOffSet; i++) {
     let singleUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
     let singleResponse = await fetch(singleUrl);
     let singleResponseAsJson = await singleResponse.json();
     pokemonMap[i] = singleResponseAsJson;
-
+    currentPokemon = singleResponseAsJson["name"];
+    allFetchedPokemon.push(currentPokemon);
     let pokemonCards = document.getElementById("pokemon-cards");
     pokemonCards.innerHTML += renderSinglePokemon(i);
   }
@@ -37,62 +54,61 @@ async function loadPokemon() {
 
 function renderSinglePokemon(i) {
   let currentPokemon = pokemonMap[i]["name"];
-  let currentPokemonImageSrc = pokemonMap[i]["sprites"]["other"]["official-artwork"]["front_default"];
+  let currentPokemonImageSrc =
+    pokemonMap[i]["sprites"]["other"]["official-artwork"]["front_default"];
   let currentPokemonImageSrcLittle = pokemonMap[i]["sprites"]["front_default"];
 
   capitalizedType =
     currentPokemon.charAt(0).toUpperCase() + currentPokemon.slice(1);
-    renderSinglePokemonCard(i, currentPokemonImageSrcLittle, capitalizedType, currentPokemonImageSrc);
   return `
     <div onclick="showPopup(${i})" class="pokemon-single-card" id="pokemon-single-card-${i}">
         <img class="pokemon-image-little" src="${currentPokemonImageSrcLittle}"/>
         <h2>${capitalizedType}</h2>
         <img class="pokemon-image" src="${currentPokemonImageSrc}"/>
-        <div id="types" class="types-container types-desc-wrapper">${getTypesHtml(i)}</div>
+        <div id="types" class="types-container types-desc-wrapper">${getTypesHtml(
+          i
+        )}</div>
     </div>
     `;
 }
 
 function getTypesHtml(i) {
   const typeColors = {
-    grass: 'bgGreen',
-    fire: 'bgRed',
-    water: 'bgBlue',
-    bug: 'bgLiteGreen',
-    normal: 'bgGray',
-    poison: 'bgPurple',
-    flying: 'bgLitePurple',
-    electric: 'bgYellow',
-    ground: 'bgOrange',
+    grass: "bgGreen",
+    fire: "bgRed",
+    water: "bgBlue",
+    bug: "bgLiteGreen",
+    normal: "bgGray",
+    poison: "bgPurple",
+    flying: "bgLitePurple",
+    electric: "bgYellow",
+    ground: "bgOrange",
   };
 
   let templateText = "";
 
   for (let j = 0; j < pokemonMap[i]["types"].length; j++) {
     let currentPokemonType = pokemonMap[i]["types"][j]["type"]["name"];
-
     if (typeColors[currentPokemonType]) {
       templateText += `<div class="types popupflex ${typeColors[currentPokemonType]}">${currentPokemonType}</div>`;
     }
   }
-
   return templateText;
 }
 
 function getStats(i) {
-    let templateText = "";
+  let templateText = "";
 
-    for (let l = 0; l < pokemonMap[i]["stats"].length; l++) {
-        let currentPokemonValue = pokemonMap[i]["stats"][l]["base_stat"];
-        let currentPokemonName = pokemonMap[i]["stats"][l]["stat"]["name"];
-        templateText += `
+  for (let l = 0; l < pokemonMap[i]["stats"].length; l++) {
+    let currentPokemonValue = pokemonMap[i]["stats"][l]["base_stat"];
+    let currentPokemonName = pokemonMap[i]["stats"][l]["stat"]["name"];
+    templateText += `
         <div class="stats">${currentPokemonName}</div>
         <progress id="progressBar" max="100" value="${currentPokemonValue}"><span></span></progress>
 
         `;
-    }
-  
-    return templateText;
+  }
+  return templateText;
 }
 function getAbility(i) {
   let templateText = "";
@@ -103,7 +119,6 @@ function getAbility(i) {
 
     templateText += `<div class="ability popupflex">${currentPokemonAbility}</div>`;
   }
-
   return templateText;
 }
 
@@ -141,4 +156,3 @@ function closePopup() {
   document.getElementById("container").classList.remove("popup-open");
   document.getElementById("single-card").classList.remove("single-bg-card");
 }
-
--- template.js
+++ template.js
@@ -1,23 +1,33 @@
-function popupSingleCard(i, currentPokemonImageSrcLittle, capitalizedType, currentPokemonImageSrc, currentPokemonHeight, currentPokemonWeight){
-    return `
+function popupSingleCard(
+  i,
+  currentPokemonImageSrcLittle,
+  capitalizedType,
+  currentPokemonImageSrc,
+  currentPokemonHeight,
+  currentPokemonWeight
+) {
+  return `
       <div id="popup-container" class="single-card">
         <div id="pokemon-popup" class="pokemon-popup">
-        <b>Pokemon Number</b> ${i}
-          <div class="popupflex">
-          <p><b>Height</b>: ${currentPokemonHeight}</p>
-          <p><b>Weight</b>: ${currentPokemonWeight}</p>
-        </div>
-          <img class="pokemon-image-little" src="${currentPokemonImageSrcLittle}"/>
-          <h2>${capitalizedType}</h2>
-          <img class="pokemon-image" src="${currentPokemonImageSrc}"/>
+          <img class="desktop-hide" src="${currentPokemonImageSrcLittle}"/>
+          <div class="pokemon-image-little">
+          <img onclick ="closePopup()" class="pokemon-mobile-close" src="./img/close.png"/>
+          <img class="pokemon-mobile" src="${currentPokemonImageSrcLittle}"/>
+          </div>          
+          <h2 class="h2-popup">${capitalizedType}</h2>
+          <img class="pokemon-image pokemon-image-popup" src="${currentPokemonImageSrc}"/>
           <div class="tab-btns">
-          <button id="tab1" class="btn-primary tab-active" onclick="getTabInfo(${i})">Type/Abilities</button>
+          <button id="tab1" class="btn-primary tab-active" onclick="getTabInfo(${i}, ${currentPokemonHeight}, ${currentPokemonWeight})">Type/Abilities</button>
         <button id="tab2" class="btn-primary" onclick="getTabStats(${i})">Open Stats</button>
         </div>
           <div id="pokemon-tab-info" class="pokemon-tab-info">
-
+          <h3>Pokemon Number #${i}</h3> 
+            <div class="height-weight-wrapper">
+              <p><b>Height</b>: ${currentPokemonHeight}</p>
+              <p><b>Weight</b>: ${currentPokemonWeight}</p>
+            </div>
           <h3>Type</h3>
-          <div id="popup-pokemon-type" class="types-container popupflex types-popup-wrapper">
+          <div id="popup-pokemon-type" class="popupflex types-container">
             ${getTypesHtml(i)}
           </div>
           <h3>Abilites</h3>
@@ -28,15 +38,18 @@ function popupSingleCard(i, currentPokemonImageSrcLittle, capitalizedType, curre
           </div>
         </div>
       </div>
-    `
+    `;
 }
 
-function getTabInfo(i) {
-    // document.getElementById("pokemon-tab-info").innerHTML = "";
-    document.getElementById("tab1").classList.add("tab-active");
-    document.getElementById("tab2").classList.remove("tab-active");
-    document.getElementById("pokemon-tab-info").innerHTML = 
-    `
+function getTabInfo(i, currentPokemonHeight, currentPokemonWeight) {
+  document.getElementById("tab1").classList.add("tab-active");
+  document.getElementById("tab2").classList.remove("tab-active");
+  document.getElementById("pokemon-tab-info").innerHTML = `
+  <h3>Pokemon Number #${i}</h3> 
+  <div class="height-weight-wrapper">
+    <p><b>Height</b>: ${currentPokemonHeight}</p>
+    <p><b>Weight</b>: ${currentPokemonWeight}</p>
+  </div>
           <h3>Type</h3>
           <div id="popup-pokemon-type" class="popupflex types-container">
             ${getTypesHtml(i)}
@@ -50,23 +63,12 @@ function getTabInfo(i) {
 }
 
 function getTabStats(i) {
-    document.getElementById("tab1").classList.remove("tab-active");
-    document.getElementById("tab2").classList.add("tab-active");
-    document.getElementById("pokemon-tab-info").innerHTML = 
-    `
+  document.getElementById("tab1").classList.remove("tab-active");
+  document.getElementById("tab2").classList.add("tab-active");
+  document.getElementById("pokemon-tab-info").innerHTML = `
     <h3>Stats</h3>
     <div class="progress">
     ${getStats(i)}
-    </div>
-    `;
-}
-function renderSinglePokemonCard(i, currentPokemonImageSrcLittle, capitalizedType, currentPokemonImageSrc) {
-    return `
-    <div onclick="showPopup(${i})" class="pokemon-single-card" id="pokemon-single-card-${i}">
-        <img class="pokemon-image-little" src="${currentPokemonImageSrcLittle}"/>
-        <h2>${capitalizedType}</h2>
-        <img class="pokemon-image" src="${currentPokemonImageSrc}"/>
-        <div id="types" class=" types-container types-desc-wrapper">${getTypesHtml(i)}</div>
     </div>
     `;
 }
